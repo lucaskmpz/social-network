668244a2c5a42f1900403ec01825199d64d8d9b3
Lucas <lucasclmaia0@gmail.com>
2023-09-07 17:02:33 -0300

    feat comments section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
-import { Sidebar } from "./components/Sidebar"
+import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
 
@@ -12,19 +12,18 @@ export function App() {
       <Header />
 
       <div className={styles.wrapper}>
-      <Sidebar />
+        <Sidebar />
         <main>
           <Post
-						  author="Diego Fernandes"
-						  content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consectetur inventore veritatis excepturi illo? Dolorem nesciunt odit porro! Debitis dolorem, fuga ullam laboriosam minima voluptatibus commodi repellendus nobis impedit aliquam."
-					/>
-					<Post
-						author="Diego Fernandes"
-						content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consectetur inventore veritatis excepturi illo? Dolorem nesciunt odit porro! Debitis dolorem, fuga ullam laboriosam minima voluptatibus commodi repellendus nobis impedit aliquam."
-					/>
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consectetur inventore veritatis excepturi illo? Dolorem nesciunt odit porro! Debitis dolorem, fuga ullam laboriosam minima voluptatibus commodi repellendus nobis impedit aliquam."
+          />
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consectetur inventore veritatis excepturi illo? Dolorem nesciunt odit porro! Debitis dolorem, fuga ullam laboriosam minima voluptatibus commodi repellendus nobis impedit aliquam."
+          />
         </main>
       </div>
     </div>
-  )
+  );
 }
-
---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/lucaskmpz.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Lucas Maia</strong>
+              <time
+                title="07 de Setembro às 11:41h"
+                dateTime="2023-09-07 11:41:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Auplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
