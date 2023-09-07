2e2e69cb3badc1d7bb2a76a963ab8f40e12edfa1
Lucas <lucasclmaia0@gmail.com>
2023-09-07 17:51:37 -0300

    feat responsiveness

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,6 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
+
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -6,11 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/lucaskmpz.png"
-            alt=""
-          />
+          <Avatar src="https://github.com/lucaskmpz.png" />
           <div className={styles.authorInfo}>
             <strong>Lucas Maia</strong>
             <span>Web Developer</span>
