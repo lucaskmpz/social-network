ff1f18920d0d135e1287291e80383acbad30d64f
Lucas <lucasclmaia0@gmail.com>
2023-09-08 20:32:31 -0300

    feat date and map

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/lucaskmpz.png",
+      name: "Lucas Maia",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-09-07 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/wicarpessoa.png",
+      name: "Wicar Pessoa",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-09-07 21:00:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -14,14 +53,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Diego Fernandes"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consectetur inventore veritatis excepturi illo? Dolorem nesciunt odit porro! Debitis dolorem, fuga ullam laboriosam minima voluptatibus commodi repellendus nobis impedit aliquam."
-          />
-          <Post
-            author="Diego Fernandes"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consectetur inventore veritatis excepturi illo? Dolorem nesciunt odit porro! Debitis dolorem, fuga ullam laboriosam minima voluptatibus commodi repellendus nobis impedit aliquam."
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
